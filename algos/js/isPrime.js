080a5dbb96be04c936b6c5eaa4aed75b5c02e8e5
Dan <drbrown01@yahoo.com>
2017-08-19 11:55:07 -0500

    added algo pages

---
+++ algos/js/isPrime.js
@@ -0,0 +1,31 @@
+
+//document.ready
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+// listen for input from user for value
+$('click').click(funcion(){
+  .html("")
+});
+//check is number
+
+//place for number..div num
+
+
+//value is user input
+var value = $(num selected by user);
+
+function isPrime(value) {
+    for(var i = 2; i < value; i++) {
+        if(value % i === 0) {
+            return false;
+        }
+    }
+    return value > 1;
+}
+
+isPrime();
+
+
+
+});
